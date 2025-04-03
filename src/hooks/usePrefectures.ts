df3a505de12e2b0db5429a6d354c96c08754e3b0
momominamihinata <chocohina.4673@gmail.com>
2025-04-03 17:04:05 +0900

    refactor: 型定義のファイルパス変更

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Prefecture } from '@/types/prefecture';
+import { Prefecture } from '@/types/types';
 import { fetchPrefectures } from '@/services/prefectureApi';
 
 /**

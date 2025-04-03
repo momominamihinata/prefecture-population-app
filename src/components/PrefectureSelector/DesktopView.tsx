2618cb7f99d8e51692b8431e2963e1464b1e7a90
momominamihinata <chocohina.4673@gmail.com>
2025-04-03 18:01:45 +0900

    #30 feat: ホバー時の動きを共通に

--- src/components/PrefectureSelector/DesktopView.tsx
+++ src/components/PrefectureSelector/DesktopView.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import JapanMap from './JapanMap';
 import RegionBlock from './RegionBlock';
-import { DesktopViewProps } from './types';
-import { REGION_DATA, REGION_POSITIONS } from './constants';
+import { DesktopViewProps } from '@/types/types';
+import { REGION_DATA, REGION_POSITIONS } from '@/types/constants';
 
 const DesktopView: React.FC<DesktopViewProps> = ({
   loading,
   isPrefectureSelected,
   onPrefectureClick,
   getPrefecturesByRegion,
+  hoveredPrefCode,
+  onPrefectureHover
 }) => {
   return (
-    <div className="hidden md:block relative h-[700px]">
+    <div className="hidden lg:block relative h-[700px] mb-28">
       {/* 地図 */}
       <div className="absolute left-[51%] top-1/2 -translate-x-1/2 -translate-y-1/2 w-[130%] flex items-center justify-center z-0">
         <JapanMap
           onPrefectureClick={onPrefectureClick}
           isPrefectureSelected={isPrefectureSelected}
+          hoveredPrefCode={hoveredPrefCode}
+          onPrefectureHover={onPrefectureHover}
         />
       </div>
       
@@ -34,6 +38,8 @@ const DesktopView: React.FC<DesktopViewProps> = ({
               loading={loading}
               isPrefectureSelected={isPrefectureSelected}
               onPrefectureClick={onPrefectureClick}
+              hoveredPrefCode={hoveredPrefCode}
+              onPrefectureHover={onPrefectureHover}
             />
           </div>
         );
